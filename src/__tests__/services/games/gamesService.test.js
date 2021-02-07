c0b2bfae9f01fcdac4eb2424726fe0ebf87a1d44
Rafael <rafaelsilvamap@gmail.com>
2021-02-07 17:14:02 -0300

    Creating test function getFile success and test function getFile fail for pathName or fileName invalid.

---
+++ src/__tests__/services/games/gamesService.test.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const GamesService = require('../../../services/games/gamesServices');
+
+const service = new GamesService();
+describe('GamesService.js file test suite', () => {
+  test('Test function getFile success', () => {
+    const pathName = path.resolve(__dirname, '..', '..', '..', 'data');
+    const fileName = 'games.log';
+    const response = service.getFile({
+      pathName,
+      fileName,
+    });
+
+    expect(response.error).toBe(false);
+  });
+
+  test('Test function getFile fail for pathName or fileName invalid', () => {
+    const pathName = path.resolve(__dirname, 'data');
+    const fileName = 'games.log';
+    const response = service.getFile({
+      pathName,
+      fileName,
+    });
+
+    expect(response.error).toBe(true);
+  });
+});
