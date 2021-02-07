b6df55b104874971da0b1143ff9278a37b7711dc
Rafael <rafaelsilvamap@gmail.com>
2021-02-07 16:46:12 -0300

    Creating test function initConvertLogToJson fail for pathName or fileName invalid .

--- src/__tests__/controllers/games/gamesController.test.js
+++ src/__tests__/controllers/games/gamesController.test.js
@@ -13,4 +13,15 @@ describe('GamesController.js file test suite', () => {
     expect(response.error).toBe(false);
     expect(response.file.length).toEqual(21);
   });
+
+  test('Test function initConvertLogToJson fail for pathName or fileName invalid', () => {
+    const pathName = path.resolve(__dirname, 'data');
+    const fileName = 'games.log';
+    const response = gamesController.initConvertLogToJson({
+      pathName,
+      fileName,
+    });
+
+    expect(response.error).toBe(true);
+  });
 });
